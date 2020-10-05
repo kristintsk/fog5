ecd076f7deef408a2a9a246c3d721795f17671af
Kristin <Kristin@KRISTIN-PC>
2020-10-05 14:49:52 +0300

    add changeColor.js

---
+++ js/changeColor.js
@@ -0,0 +1,8 @@
+function upDate(changeColor) {
+    /* document.getElementById('citeCap').style.color = "#7fbdff"; */
+    document.getElementById('citeCap').style.color = "lime";
+}
+
+function unDo() {
+    document.getElementById('citeCap').style.color = "";
+}
