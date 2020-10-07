ed0827933cf95cf49f0623bc1ed9e2a63e69d4b6
Kristin <Kristin@KRISTIN-PC>
2020-10-07 13:04:32 +0300

    add js for masthead bg

--- js/changeColor.js
+++ js/changeColor.js
@@ -1,8 +1,9 @@
-function upDate(changeColor) {
-    /* document.getElementById('citeCap').style.color = "#7fbdff"; */
-    document.getElementById('citeCap').style.color = "lime";
+function upDate(changeBg) {
+    /* document.getElementById('mastheadBg').style.background = "#3b444b"; */
+    /* document.getElementById('mastheadBg').style.background = "rgba(59, 68, 75, 1.9)"; */
+    document.getElementById('mastheadBg').style.background = "url('assets/img/bg-masthead2.jpg') no-repeat";
 }
 
 function unDo() {
-    document.getElementById('citeCap').style.color = "";
+    document.getElementById('mastheadBg').style.background = "";
 }
