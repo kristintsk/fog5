c03cfd0c5a722fb8213b769a501e0a10a70bb679
Kristin <Kristin@KRISTIN-PC>
2020-10-18 18:19:06 +0300

    delete unnecessary css, update html and js

--- js/changeBg.js
+++ js/changeBg.js
@@ -1,6 +1,4 @@
 function upDate(changeBg) {
-    /* document.getElementById('mastheadBg').style.background = "#3b444b"; */
-    /* document.getElementById('mastheadBg').style.background = "rgba(59, 68, 75, 1.9)"; */
     document.getElementById('mastheadBg').style.background = "url('assets/img/bg-masthead2.jpg') no-repeat";
 }
 
